6497853e027b25e213cc186c535ee9249559afd2
optarou <optarou.fe@gmail.com>
2018-11-07 21:32:25 +0900

    work画像表示機能追加

--- pages/works.vue
+++ pages/works.vue
@@ -9,6 +9,7 @@
             <dd class="detail">{{ item.detail }}</dd>
             <dd class="tools">Tools: {{ toolsList(item.tools) }}</dd>
             <dd class="date">{{ item.date }}</dd>
+            <dd class="capture" :style="{ backgroundImage: 'url(' + require(`@/assets/image/${item.capture}`) + ')' }" v-if="item.capture"></dd>
           </dl>
         </a>
       </li>
@@ -24,6 +25,7 @@ export default {
         // {
         //   title: 'Preshare',
         //   detail: 'あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ',
+        //   capture: '',
         //   tools: ['Vue.js', 'Firebase'],
         //   url: 'https://google.com',
         //   date: '2018/11'
@@ -31,6 +33,7 @@ export default {
         {
           title: 'J.M.Portfolio',
           detail: 'ポートフォリオサイト',
+          capture: 'work1.png',
           tools: ['Vue.js', 'Nuxt.js'],
           url: '/',
           date: '2018/11'
@@ -38,6 +41,7 @@ export default {
         // {
         //   title: 'Resister Calendar',
         //   detail: 'Vanilla JavaScriptでVMMVパターンで設計しました。',
+        //   capture: '',
         //   tools: ['Vanilla JavaScript'],
         //   url: 'https://google.com',
         //   date: '2018/10'
@@ -45,6 +49,7 @@ export default {
         // {
         //   title: 'Movie Carousel',
         //   detail: 'YoutubeIframeAPIを利用した動画入りカルーセルです。',
+        //   capture: '',
         //   tools: ['jQuery', 'YoutubeIframeAPI', 'File API'],
         //   url: 'https://google.com',
         //   date: '2017/12'
@@ -52,6 +57,7 @@ export default {
         // {
         //   title: 'Modal',
         //   detail: '選択したDOMから情報を取得してモーダル上に表示させています。',
+        //   capture: '',
         //   tools: ['jQuery'],
         //   url: 'https://google.com',
         //   date: '2018/6'
@@ -59,12 +65,14 @@ export default {
         // {
         //   title: 'Usage tools by Vue.js',
         //   detail: '個人用Web給与明細確認ツール。ローカルに保存していたWeb給与明細のページをiframeに表示させます。サイドナビから見たい年月を選択して表示している明細情報を変更できます。初めてVue.jsに触れた時に練習で作りました。',
+        //   capture: '',
         //   tools: ['Vue.js'],
         //   date: '2018/3'
         // },
         // {
         //   title: 'Develop tool by jQuery',
         //   detail: '同一性ポリシーを利用してiframe内のページを別タブで開く際にHTMLを操作して当てるスタイルシートを変更できます。',
+        //   capture: '',
         //   tools: ['jQuery'],
         //   date: '2017/3'
         // },
@@ -89,6 +97,9 @@ export default {
     transition: all .2s ease-out;
     &:hover {
       background-color: rgba(118, 219, 209, .8);
+      .capture {
+        opacity: .8;
+      }
     }
     + li {
       margin-top: 10px;
@@ -107,6 +118,16 @@ dd {
 .detail {
   font-size: 80%;
   line-height: 1.4;
+}
+
+.capture {
+  width: 100%;
+  height: 100px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center center;
+  transform: translateZ(0);
+  transition: all .2s ease-out;
 }
 
 .tools {
