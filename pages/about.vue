903457d58a142090e1b36bfea7b1110853f26de6
optarou <optarou.fe@gmail.com>
2018-11-08 20:50:09 +0900

    update: テキスト追加

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="about">
     <h2>About me</h2>
     <ul class="profiles">
       <li v-for="item in profiles" :key="item.title">
@@ -88,7 +88,7 @@ var skills = [
     detail: ``
   },
   { level: 70, title: 'Visual Studio Code',
-    detail: `\<- Atom \<- Sublime3`
+    detail: `Atom, SublimeText3`
   },
   { level: 8, title: 'AWS',
     detail: `S3で静的サイトを公開`
@@ -102,37 +102,38 @@ var careers = [
     start: '2016/11',
     end: '',
     title: 'SES',
-    detail: `大規模飲食系BtoBサービスを展開している企業に常駐。<br/>飲食店向けホームページ作成サービスのフロントエンドを担当。業務内容はサービスのエンハンス・リファクタリング。<br/>担当制作例: 予約管理カレンダー・YouTube動画を組み込み可能なカルーセル・汎用モーダルウィンドウ・複数静的ページ管理画面<br/>特筆事項: 機能制作のほかに、<a href="https://www.oreilly.co.jp/books/9784873115658/" target="_blank">リーダブルコード</a>を参考にして数千行リファクタリングを行った経験。`
+    detail: `大規模飲食系BtoBサービスを展開している企業に常駐。飲食店向けホームページ作成サービスのフロントエンドを担当。業務内容はサービスのエンハンス・リファクタリング。<br/>担当制作例: 予約管理カレンダー・YouTube動画を組み込み可能なカルーセル・汎用モーダルウィンドウ・複数静的ページ管理画面<br/>特筆事項: 機能制作のほかに、<a href="https://www.oreilly.co.jp/books/9784873115658/" target="_blank">リーダブルコード</a>を参考にして数千行リファクタリングを行った経験。`
   },
   {
     start: '2016/5',
     end: '',
     title: 'Lei Hau\'oli Co.,Ltd.',
-    detail: `詳細`
-    // detail: `フロントエンドエンジニアフロントエンドエンジニアフロントエンドエンジニアフロントエンドエンジニアフロントエンドエンジニア 中規模Webサイト作成`
+    detail: `中規模受託WEB制作会社にフロントエンドエンジニアとして所属。社内でJavaScript研修を受けた後、小中規模の静的サイトの制作などを担う。<br/>制作例: 大手化粧品メーカーコーポレートサイト...`
   },
   {
     start: '2016/1',
     end: '2016/5',
     title: 'In preparation',
-    detail: `詳細`
-    // detail: `サイ本などを購入してJavaScriptを勉強していましたサイ本などを購入してJavaScriptを勉強していましたサイ本などを購入してJavaScriptを勉強していましたサイ本などを購入してJavaScriptを勉強していました`
+    detail: `休職。JavaScriptを勉強するためMacBookProと本を購入。<br/>参考にしていた本:
+<a href="https://www.oreilly.co.jp/books/9784873115733/" target="_blank">JavaScript第6版</a>・
+<a href="https://www.oreilly.co.jp/books/9784873113913/" target="_blank">JavaScript: The Good Parts</a>・
+<a href="https://www.oreilly.co.jp/books/9784873116211/" target="_blank">開眼! JavaScript</a>・
+<a href="https://www.oreilly.co.jp/books/9784873114880/" target="_blank">JavaScriptパターン</a>・
+<a href="https://www.amazon.co.jp/Web%E5%88%B6%E4%BD%9C%E8%80%85%E3%81%AE%E3%81%9F%E3%82%81%E3%81%AECSS%E8%A8%AD%E8%A8%88%E3%81%AE%E6%95%99%E7%A7%91%E6%9B%B8-%E3%83%A2%E3%83%80%E3%83%B3Web%E9%96%8B%E7%99%BA%E3%81%AB%E6%AC%A0%E3%81%8B%E3%81%9B%E3%81%AA%E3%81%84%E3%80%8C%E4%BF%AE%E6%AD%A3%E3%81%97%E3%82%84%E3%81%99%E3%81%84CSS%E3%80%8D%E3%81%AE%E8%A8%AD%E8%A8%88%E6%89%8B%E6%B3%95-%E8%B0%B7-%E6%8B%93%E6%A8%B9/dp/4844336355" target="_blank">Web制作者のためのCSS設計の教科書</a>・
+<a href="https://www.oreilly.co.jp/books/9784873115658/" target="_blank">リーダブルコード</a><br/>
+勉強会で知り合った人の企業から内定をもらう。`
   },
   {
     start: '2015/4',
     end: '2015/12',
     title: 'Dawn.Inc',
-    detail: `詳細`
-    // detail: `マークアップエンジニア
-    // LP/小規模Webサイト作成小規模Webサイト作成小規模Webサイト作成小規模Webサイト作成小規模Webサイト作成
-    // WordPress実装`
+    detail: `小規模受託WEB制作会社に新卒入社。マークアップエンジニアとして所属。HTML/CSSでのコーディングをひたすら行う。業務内容はLPサイト制作や小規模コーポレートサイトのWordPress組み込みなど。社内で浸透していなかったSassの導入を提案。上京を機に外部の勉強会に積極的に参加するようになる。より技術的に成長できる環境を求め退職。`
   },
   {
     start: '2011/4',
     end: '2015/3',
     title: 'University of Aizu',
-    detail: `詳細`
-    // detail: `コンピュータ理工学部`
+    detail: `コンピュータ理工学部卒。学部3年次にJavaScriptに興味を持ち、ドットインストールで独学する。4年次にWebGL(Three.js)を用いた大学構内ストリートビューを作成。デザインに興味があり色彩検定2級を取得。絵を描く趣味のおかげでPhotoShopの操作を覚える。フロントエンドエンジニアを志し、Web制作の基礎を作る思いでマークアップエンジニアとして内定をもらう。`
   },
 ];
 
@@ -156,15 +157,6 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/assets/style/global.scss';
-
-dd {
-  a {
-    text-decoration: underline;
-    &:hover {
-      text-decoration: none;
-    }
-  }
-}
 
 .profiles {
   margin-top: .5em;
@@ -298,5 +290,15 @@ dd {
 
 .sections {
   margin-top: .5em;
+}
+</style>
+<style lang="scss">
+.about {
+  a {
+    text-decoration: underline;
+    &:hover {
+      text-decoration: none;
+    }
+  }
 }
 </style>
